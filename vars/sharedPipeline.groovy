2fd7aa97e00534d19deefd8810406fc9e5add0d1
shariqrizvi-nclouds <shariq@nclouds.com>
2020-02-04 11:40:47 -0800

    Update sharedPipeline.groovy

--- vars/sharedPipeline.groovy
+++ vars/sharedPipeline.groovy
@@ -145,8 +145,7 @@ def call(){
                             sh "\$(aws ecr get-login --no-include-email --region ${AWS_REGION})"	
                             sh "docker tag ${DOCKER_TAG_NAME} ${ECR_REPO}:${commit}"	
                             sh "docker tag ${DOCKER_TAG_NAME} ${ECR_REPO}:latest"	
-                            sh "docker push ${ECR_REPO}:${commit}"	
-                            sh "docker push ${ECR_REPO}:latest"
+                            sh "docker push ${ECR_REPO}:${commit}"
                             sh 'echo "Stage push done"'
                         }
                     }
