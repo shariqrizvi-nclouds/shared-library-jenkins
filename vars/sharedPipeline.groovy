eb0f05b69d07423c27cd770ccf070218f2640ece
shariqrizvi-nclouds <shariq@nclouds.com>
2020-02-04 13:37:06 -0800

    Removed latest tag

--- vars/sharedPipeline.groovy
+++ vars/sharedPipeline.groovy
@@ -144,9 +144,7 @@ def call(){
                         script {	
                             sh "\$(aws ecr get-login --no-include-email --region ${AWS_REGION})"	
                             sh "docker tag ${DOCKER_TAG_NAME} ${ECR_REPO}:${commit}"
-                            sh "docker tag ${DOCKER_TAG_NAME} ${ECR_REPO}:latest"
                             sh "docker push ${ECR_REPO}:${commit}"
-                            sh "docker push ${ECR_REPO}:latest"
                             sh 'echo "Stage push done"'
                         }
                     }
